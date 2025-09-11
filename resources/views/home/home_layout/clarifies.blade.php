c4a07e56877ec5fb338b76a50d7ce7282b96c76b
Enrique <enrique.sousa@gmail.com>
2025-09-11 07:23:53 -0700

    58. Home Clarifies Section Setup Part 3 - Section Clarifie

--- resources/views/home/home_layout/clarifies.blade.php
+++ resources/views/home/home_layout/clarifies.blade.php
@@ -2,17 +2,22 @@
 <section class="lonyo-section-padding6">
     <div class="container">
         <div class="row">
+
+            @php
+                $clarifie = App\Models\Clarifie::find(1);
+            @endphp
+
+            <!-- Image (302x618) -->
             <div class="col-lg-5">
                 <div class="lonyo-content-thumb" data-aos="fade-up" data-aos-duration="700">
-                    <img src="{{ asset('frontend/assets/images/v1/content-thumb.png') }}" alt="">
+                    <img src="{{ asset($clarifie->image) }}" alt="">
                 </div>
             </div>
+
             <div class="col-lg-7 d-flex align-items-center">
                 <div class="lonyo-default-content pl-50" data-aos="fade-up" data-aos-duration="700">
-                    <h2>It clarifies all strategic financial decisions</h2>
-                    <p class="data">With this tool, you can say goodbye to overspending, stay on track with your
-                        savings goals, and say goodbye to financial worries. Get ready for a clearer view of your
-                        finances like never before!</p>
+                    <h2>{{ __($clarifie->title) }}</h2>
+                    <p class="data">{{ __($clarifie->description) }}</p>
                     <div class="lonyo-faq-wrap1 mt-50">
                         <div class="lonyo-faq-item open" data-aos="fade-up" data-aos-duration="500">
                             <div class="lonyo-faq-header">
@@ -59,6 +64,7 @@
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
 </section>
