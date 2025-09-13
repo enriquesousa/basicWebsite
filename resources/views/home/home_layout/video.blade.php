8c4eca238c98fe53c0e784bce95922cc837dd07f
Enrique <enrique.sousa@gmail.com>
2025-09-13 08:41:03 -0700

    64. Home Usability Down Section Setup Part 3

--- resources/views/home/home_layout/video.blade.php
+++ resources/views/home/home_layout/video.blade.php
@@ -5,8 +5,9 @@
 <div class="lonyo-section-padding bg-heading position-relative sectionn">
     <div class="container">
         <div class="row">
-            <div class="col-lg-5">
 
+            <!-- Video Image and Play Button -->
+            <div class="col-lg-5">
                 <div class="lonyo-video-thumb">
                     <img src="{{ asset($video->image) }}" alt="">
                     <a class="play-btn video-init" href="{{ asset($video->youtube) }}">
@@ -16,8 +17,9 @@
                         <div class="waves wave-3"></div>
                     </a>
                 </div>
-
             </div>
+
+            <!-- Title, Description and Download Button -->
             <div class="col-lg-7 d-flex align-items-center">
                 <div class="lonyo-default-content lonyo-video-section pl-50" data-aos="fade-up" data-aos-duration="500">
                     <h2>{{ $video->title }}</h2>
@@ -27,57 +29,33 @@
                     </div>
                 </div>
             </div>
+
         </div>
 
+        <!-- Sección de las 3 características -->
         <div class="row">
 
-            <!-- one -->
-            <div class="col-xl-4 col-md-6">
-                <div class="lonyo-process-wrap" data-aos="fade-up" data-aos-duration="500">
-                    <div class="lonyo-process-number">
-                        <img src="{{ asset('frontend/assets/images/v1/n1.svg') }}" alt="">
-                    </div>
-                    <div class="lonyo-process-title">
-                        <h4>Connect Your Accounts</h4>
-                    </div>
-                    <div class="lonyo-process-data">
-                        <p>Link your bank, credit card or investment accounts to automatically track transactions
-                            and get a complete financial overview</p>
-                    </div>
-                </div>
-            </div>
+            @php
+                $connects = App\Models\Connect::whereIn('id',[1,2,3])->get()->keyBy('id');
+            @endphp
 
-            <!-- two -->
-            <div class="col-xl-4 col-md-6">
-                <div class="lonyo-process-wrap" data-aos="fade-up" data-aos-duration="700">
-                    <div class="lonyo-process-number">
-                        <img src="{{ asset('frontend/assets/images/v1/n2.svg') }}" alt="">
-                    </div>
-                    <div class="lonyo-process-title">
-                        <h4>Set Budgets & Goals</h4>
-                    </div>
-                    <div class="lonyo-process-data">
-                        <p>Define your spending limits and savings goals for categories like groceries, bills or
-                            future investments to stay on track.</p>
+            @foreach ($connects as $connect) 
+                <div class="col-xl-4 col-md-6">
+                    <div class="lonyo-process-wrap" data-aos="fade-up" data-aos-duration="500">
+                        <div class="lonyo-process-number">
+                            {{-- <img src="{{ asset('frontend/assets/images/v1/n1.svg') }}" alt=""> --}}
+                            <img src="{{ asset('frontend/assets/images/v1/n'.$connect->id. '.svg') }}" alt="">
+                        </div>
+                        <div class="lonyo-process-title">
+                            <h4>{{ $connect->title }}</h4>
+                        </div>
+                        <div class="lonyo-process-data">
+                            <p>{{ $connect->description }}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
 
-            <!-- three -->
-            <div class="col-xl-4 col-md-6">
-                <div class="lonyo-process-wrap" data-aos="fade-up" data-aos-duration="900">
-                    <div class="lonyo-process-number">
-                        <img src="{{ asset('frontend/assets/images/v1/n3.svg') }}" alt="">
-                    </div>
-                    <div class="lonyo-process-title">
-                        <h4>Monitor & Automate</h4>
-                    </div>
-                    <div class="lonyo-process-data">
-                        <p>Check your financial dashboard for regular updates and set up automatic payments or
-                            savings to simplify management.</p>
-                    </div>
-                </div>
-            </div>
 
             <div class="border-bottom" data-aos="fade-up" data-aos-duration="500"></div>
 
