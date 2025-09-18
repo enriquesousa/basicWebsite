7c4c22b81de0f326726214560cff2e777ddfc2bd
Enrique <enrique.sousa@gmail.com>
2025-09-18 16:48:09 -0700

    76. Show Our Team in Frontend

--- resources/views/home/team/our_team.blade.php
+++ resources/views/home/team/our_team.blade.php
@@ -29,26 +29,37 @@
             <div class="lonyo-section-title max-w616">
                 <h2>{{ __('Meet Our Team') }}</h2>
             </div>
-            <div class="row">
 
-                <div class="col-lg-3 col-md-6">
-                    <div class="lonyo-team-wrap" data-aos="fade-up" data-aos-duration="500">
-                        <div class="lonyo-team-thumb">
-                            <a href="single-team.html"><img src="{{ asset('frontend/assets/images/about-us/t1.png') }}" alt=""></a>
-                        </div>
-                        <div class="lonyo-team-content2">
-                            <a href="single-team.html">
-                                <h6>Michael Chen</h6>
-                            </a>
-                            <p>Chief Executive Officer</p>
+            @php
+                $team = App\Models\Team::latest()->get();
+            @endphp
+            <div class="row">
+                @foreach ($team as $item)
+                    <div class="col-lg-3 col-md-6">
+                        <div class="lonyo-team-wrap" data-aos="fade-up" data-aos-duration="500">
+                            <div class="lonyo-team-thumb">
+                                <a href="single-team.html">
+                                    {{-- <img src="{{ asset('frontend/assets/images/about-us/t1.png') }}" alt=""> --}}
+                                    <img src="{{ asset($item->image) }}" alt="">
+                                </a>
+                            </div>
+                            <div class="lonyo-team-content2">
+                                <a href="single-team.html">
+                                    <h6>{{ $item->name }}</h6>
+                                </a>
+                                <p>{{ $item->position }}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
+                @endforeach
+            </div>
 
-            </div>
             <div class="mt-50 team-btn" data-aos="fade-up" data-aos-duration="700">
-                <a href="contact-us.html" class="lonyo-default-btn team-btn2">Would you joint of our group?</a>
+                <a href="{{ route('home') }}" class="lonyo-default-btn team-btn2">
+                    {{ __('Would you joint of our group?') }}
+                </a>
             </div>
+
         </div>
     </section>
     <!-- end content -->
